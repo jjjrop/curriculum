abc96a8174804593496256cd70c6aa1200e7f14f
miyazawa <masamune1623@gmail.com>
2020-12-14 19:30:14 +0900

    dotenvの削除

--- dotenv.php
+++
@@ -1,6 +0,0 @@
-<?php
-define("DB_DSN", "mysql:dbname=nanablog1114_db;host=mysql10030.xserver.jp");
-define("DB_SERVER", "mysql10030.xserver.jp");
-define("DB_USERNAME", "nanablog1114_db");
-define("DB_DBNAME", "nanablog1114_db");
-define("DB_PASSWORD", "sibrin103");
